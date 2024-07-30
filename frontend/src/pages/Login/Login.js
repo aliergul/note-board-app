533c4a4dfe5c03cbf0d2b4b3b1b1d90382d4d94c
ali <ali.ergul@pusula.net.tr>
2024-07-30 16:24:59 +0300

    adding basic routing for start

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <div className="login-container">
+        <div>
+          <input placeholder="test" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
