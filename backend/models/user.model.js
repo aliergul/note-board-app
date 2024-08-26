5e7fd4ed04a271c8703bf9bd6c9f35a81c2ff123
ali <ali.ergul@pusula.net.tr>
2024-08-26 19:52:25 +0300

    create account api, testing with postman

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   fullName: { type: String },
   email: { type: String },
   password: { type: String },
-  createdOn: { type: Data, default: new Date().getTime() },
+  createdOn: { type: Date, default: new Date().getTime() },
 });
 
 module.exports = mongoose.model("User", userSchema);
