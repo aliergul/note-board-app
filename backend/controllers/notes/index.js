66c856cea00a2ce8485b6d8950a42b60c7231230
ali <ali.ergul@pusula.net.tr>
2024-08-27 15:48:19 +0300

    delete endpoint

--- backend/controllers/notes/index.js
+++ backend/controllers/notes/index.js
@@ -1,9 +1,11 @@
 const addNote = require("./addNote").addNote;
 const editNote = require("./editNote").editNote;
 const getNotes = require("./getNotes").getNotes;
+const deleteNote = require("./deleteNote").deleteNote;
 
 module.exports = {
   addNote,
   editNote,
   getNotes,
+  deleteNote,
 };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -5,5 +5,6 @@ const noteController = require("../controllers/notes/index");
 router.post("/add-note", noteController.addNote);
 router.put("/edit-note/:noteId", noteController.editNote);
 router.get("/get-notes", noteController.getNotes);
+router.delete("/delete-note/:noteId", noteController.deleteNote);
 
 module.exports = router;
