b6b05de0a97f0d41b40f93ea7294c62ed14ad3fd
ali <ali.ergul@pusula.net.tr>
2024-08-27 14:08:36 +0300

    changed folder structure

---
+++ backend/controllers/auth/createAccount.js
@@ -0,0 +1,49 @@
+const User = require("../../models/user.model");
+
+exports.createAccount = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "User already exist",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+  });
+};
---
+++ backend/controllers/auth/index.js
@@ -0,0 +1,7 @@
+const createAccount = require("./createAccount").createAccount;
+const login = require("./login").login;
+
+module.exports = {
+  createAccount,
+  login,
+};
